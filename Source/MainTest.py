72db9927298dbc3a30f888f4354fc86896f1017d
rodrigodub <Rodrigo@coltrane.bigpond>
2013-10-06 23:00:29 +0800

    datetimes… still buggy

---
+++ Source/MainTest.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+#################################################
+## Main
+## Rotina principal
+## 
+## v2.0.0
+## for ticket 
+## 
+## Rodrigo Nobrega
+## 20131002-20131006
+#################################################
+
+
+
+# import classes
+#from tkinter import *
+from Timeshift import *
+#from TimeshiftGUI import *
+
+
+# main loop
+def main():  
+	#root = Tk()
+	#root.geometry('700x400+100+100')
+	#root.title('Timeshift')
+	#app = TimeshiftGUI(master=root)
+	#app.mainloop()
+	ts = Timeshift('/Users/Rodrigo/Documents/GitHub/Timeshift/Test/Agents.of.S.H.I.E.L.D.S01E01.Pilot.WEB-DL.x264.AAC.srt',4)
+	l1 = ts.timeList('00:02:51,326 --> 00:02:53,821') 
+	print l1
+	print ts.timeShift(l1, ts.deltaTime)
+
+
+
+# main, calling main loop
+if __name__ == '__main__':
+    main()  
+
+
+
+
+
+
+
+
+
+
--- Source/Timeshift.py
+++ Source/Timeshift.py
@@ -11,6 +11,7 @@
 
 # import modules
 from datetime import *
+#from time import * 
 import os
 
 
@@ -18,73 +19,33 @@ import os
 # define class
 class Timeshift(object):
 	"""docstring for Timeshift"""
+
+	# constructor with attributes
 	def __init__(self, fileName, deltaTime):
 		super(Timeshift, self).__init__()
 		self.fileName = fileName
 		self.deltaTime = deltaTime
 		
 
+	# method timeList()
+	def timeList(self, line):
+		return line.replace(',','.').split(' --> ')
+
+
+	# method timeShift()
+	def timeShift(self, timeList, delta):
+		#return datetime.time(int(timeList[0].split(':')[0])
+		#	, int(timeList[0].split(':')[1])
+		#	, int(float(timeList[0].split(':')[2]))) 
+		return datetime.time(0,2,51)
+
+
+
+	# method parsefile()
 
 
 
 
-
-
-# define class
-class Prote:
-
-	# documentation
-	"""\
-	A simple text processor
-	Usage: Prote(<filepath/filename>,<original string>,<new string>)
-	"""
-
-	
-	# constructor with attributes
-	def __init__(self, pathfilename, string1, string2):
-		self.inputFileName = pathfilename
-		self.inputString = string1
-		self.outputString = string2
-		self.replaceString()
-		self.renaming()
-	
-
-	
-	# methods
-	
-	def autoIncrement(self):
-		if self.inputFileName[ self.inputFileName.rfind('.')-3 : self.inputFileName.rfind('.') ].isdigit():
-			return ('00' + str(int(self.inputFileName[self.inputFileName.rfind('.')-3:self.inputFileName.rfind('.')]) + 1))[-3:]
-		else:
-			return '001'
-			
-	
-	def outputFileName(self):
-		return self.inputFileName[:-4] + '_temp'
-
-
-	def	replaceString(self):
-		# open files
-		inputFile = open(self.inputFileName,'r')
-		outputFile = open(self.outputFileName(),'w')
-		# iterate
-		for eachLine in inputFile:
-			outputFile.write(eachLine.replace(self.inputString,self.outputString))
-		# close files
-		outputFile.close()
-		inputFile.close()
-
-		
-	def timecomb(self):
-		comb = str(datetime.now().year) + ('0' + str(datetime.now().month))[-2:] + ('0' + str(datetime.now().day))[-2:] + ('0' + str(datetime.now().hour))[-2:] + ('0' + str(datetime.now().minute))[-2:] + ('0' + str(datetime.now().second))[-2:]
-		return comb
-	
-	
-	def renaming(self):
-		os.rename(self.inputFileName, self.inputFileName[:-4] + '_' + self.timecomb() + '.bak')
-		os.rename(self.outputFileName(), self.inputFileName)
-		
-		
 
 
 
